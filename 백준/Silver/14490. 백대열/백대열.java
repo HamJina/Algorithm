e9742cd8aa6e7e78ff7113a9a474b41edce8dd8b
Jina Ham <156528356+HamJina@users.noreply.github.com>
2025-01-12 21:14:40 +0900

    [Silver V] Title: 백대열, Time: 124 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Silver/14490. 백대열/백대열.java
+++ 백준/Silver/14490. 백대열/백대열.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -15,22 +15,16 @@ public class Main {
         String[] s = input.split(":");
         int m = Integer.parseInt(s[0]);
         int n = Integer.parseInt(s[1]);
-        int min = Integer.min(m, n);
+        int min = Integer.min(m,n);
+        int max = Integer.max(m,n);
+        int gcd = gcd(min, max);
 
-        List<Integer> nums = new ArrayList<>();
-        for (int i = min; i > 0 ; i--) {
-            if(min % i == 0) nums.add(i);
-        }
+        System.out.println(m/gcd+":"+n/gcd);
 
-        for (Integer num : nums) {
-            if(m%num == 0 && n%num == 0){
-                divided = num;
-                break;
-            }
-        }
+    }
 
-        String answer = String.valueOf(m/divided)+":"+String.valueOf(n/divided);
-        System.out.println(answer);
-
+    static int gcd(int a, int b) {
+        if(b%a == 0) return a;
+        else return gcd(b%a, a);
     }
 }
