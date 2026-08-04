ef15f9babb24f494f920f45327729b3eec71f531
Jina Ham <156528356+HamJina@users.noreply.github.com>
2026-08-04 12:15:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.66 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,31 +1,37 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+         // Hash에서 키 값은 고유한 값이어야 하므로 동명이인으로 인해 키값만으로 동명이인을 구분할 수 없다
+        // 키: 이름, 값: 몇명 을 활용한다. 
         HashMap<String, Integer> map = new HashMap<>();
-        for (String s : completion) {
-            if(map.containsKey(s)) {
-                Integer value = map.get(s);
-                map.put(s, value+1);
-            }else {
-                map.put(s, 1);
+        
+        for(String c : completion) {
+            if(map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                // 아직 포함되어 있지 않으면 초기화 
+                map.put(c, 1);
             }
         }
-
-        String answer = "";
-        for (String s : participant) {
-            if(map.containsKey(s)) {
-                Integer value = map.get(s);
-                if(value.equals(1)) map.remove(s);
-                else map.put(s, value-1);
+        
+        for(String p : participant) {
+            if(map.containsKey(p)) {
+                Integer value = map.get(p);
+                
+                if(value == 1) {
+                    map.remove(p);
+                } else {
+                    map.put(p, value - 1);
+                }
             } else {
-                answer = s;
+                answer = p; 
                 break;
             }
         }
-
+        
         return answer;
+        
     }
 }
